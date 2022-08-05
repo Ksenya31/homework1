c7a309e3b6e838a83d886034788383708f23981c
ksenya <kseniakalinnikova268@gmail.com>
2022-08-05 13:47:55 +0600

    added branch testing

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+        CashbackHackService service = new CashbackHackService();
+
+    @org.testng.annotations.BeforeMethod
+            public void shouldReturnNull() {
+            int actual = service.remain(900);
+            int expected = 100;
+            Assert.assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+            public void shouldReturn() {
+            int actual = service.remain(800);
+            int expected = 200;
+            Assert.assertEquals(actual, expected);
+
+    }
+}
