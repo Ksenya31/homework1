3ada88a95a622c2ac3f5c5e783973036d4aa1e01
ksenya <kseniakalinnikova268@gmail.com>
2022-08-05 16:36:11 +0600

    change faile testing

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,19 +6,20 @@ import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-        CashbackHackService service = new CashbackHackService();
+    CashbackHackService service = new CashbackHackService();
 
     @org.testng.annotations.BeforeMethod
-            public void shouldReturnNull() {
-            int actual = service.remain(900);
-            int expected = 100;
-            Assert.assertEquals(actual, expected);
+    public void shouldReturnNull() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
     }
+
     @org.testng.annotations.Test
-            public void shouldReturn() {
-            int actual = service.remain(800);
-            int expected = 200;
-            Assert.assertEquals(actual, expected);
+    public void shouldReturn() {
+        int actual = service.remain(800);
+        int expected = 200;
+        Assert.assertEquals(actual, expected);
 
     }
 }
