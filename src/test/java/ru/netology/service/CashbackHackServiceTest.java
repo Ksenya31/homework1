efa47d285cc125b73919a3b845b7ab1702f5bb17
ksenya <kseniakalinnikova268@gmail.com>
2022-08-05 15:23:40 +0600

    added branch junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,24 +1,17 @@
 package ru.netology.service;
 
-import org.testng.Assert;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
-        CashbackHackService service = new CashbackHackService();
-
-    @org.testng.annotations.BeforeMethod
-            public void shouldReturnNull() {
-            int actual = service.remain(900);
-            int expected = 100;
-            Assert.assertEquals(actual, expected);
-    }
-    @org.testng.annotations.Test
-            public void shouldReturn() {
-            int actual = service.remain(800);
-            int expected = 200;
-            Assert.assertEquals(actual, expected);
-
+    @org.junit.Test
+    public void shouldReturnNull() {
+        int actual = service.remain(9990);
+        int expected = 10;
+        Assert.assertEquals(expected,actual);
     }
 }
